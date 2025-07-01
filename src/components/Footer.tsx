114d6995850010a6980a7df40125a13cb75f8abe
David Castro <davidernesto.castroanaya@gmail.com>
2025-07-01 00:24:58 -0600

    Update Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import {Twitter, Facebook, Linkedin, Instagram, Phone, Mail, MapPin } from 'luci
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-900 text-white py-12">
+    <footer className="bg-[#1C1C1C] text-white py-12">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="grid md:grid-cols-3 gap-8">
           <div>
